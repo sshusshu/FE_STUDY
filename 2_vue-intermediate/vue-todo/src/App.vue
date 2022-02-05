0a11a192a23f4a63e599d37f012bc2029722c5f2
melody_lim <melody_lim@gccompany.co.kr>
2022-02-05 23:22:12 +0900

    ADD : Modal.vue 추가

--- 2_vue-intermediate/vue-todo/src/App.vue
+++ 2_vue-intermediate/vue-todo/src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <todo-header></todo-header>
     <todo-input v-on:addTodo="addOneTodo"></todo-input>
-    <todo-list 
-      v-bind:todoItems="todoItems" 
-      v-on:removeTodo="removeOneItem" 
+    <todo-list
+      v-bind:todoItems="todoItems"
+      v-on:removeTodo="removeOneItem"
       v-on:toggleTodo="toggleOneItem"
     ></todo-list>
     <todo-footer v-on:clearItems="clearAllItems"></todo-footer>
@@ -12,59 +12,60 @@
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
-import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TodoHeader,
     TodoInput,
     TodoList,
-    TodoFooter
+    TodoFooter,
   },
-  data(){
-    return{
-      todoItems:[],
-    }
+  data() {
+    return {
+      todoItems: [],
+    };
   },
-  methods:{
-    addOneTodo(todoItem){
-      const itemObj = {completed:false,item:todoItem};
+  methods: {
+    addOneTodo(todoItem) {
+      const itemObj = { completed: false, item: todoItem };
       this.todoItems.push(itemObj);
       localStorage.setItem(todoItem, JSON.stringify(itemObj));
     },
-    removeOneItem(todoItem,index){
-      this.todoItems.splice(index,1);
+    removeOneItem(todoItem, index) {
+      this.todoItems.splice(index, 1);
       localStorage.removeItem(todoItem.item);
     },
-    toggleOneItem(todoItem, index){
-      this.todoItems[index].completed = !this.todoItems[index].completed
+    toggleOneItem(todoItem, index) {
+      this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item,JSON.stringify(todoItem));
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItems: function() {
+    clearAllItems: function () {
       this.todoItems = [];
       localStorage.clear();
-    }
+    },
   },
-  created:function(){
-    if(localStorage.length<=0) return;
-    for(var i =0; i<localStorage.length;i++){
-      if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-      const itemObj = localStorage.getItem(localStorage.key(i));
-      this.todoItems.push(JSON.parse(itemObj));
+  created: function () {
+    if (localStorage.length <= 0) return;
+    for (var i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+        this.todoItems.push(
+          JSON.parse(localStorage.getItem(localStorage.key(i)))
+        );
       }
     }
   },
-}
+};
 </script>
 <style>
 body {
   text-align: center;
-  background-color: #F6F6F8;
+  background-color: #f6f6f8;
 }
 input {
   border-style: groove;
@@ -74,6 +75,6 @@ button {
   border-style: groove;
 }
 .shadow {
-  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
--- 2_vue-intermediate/vue-todo/src/components/TodoInput.vue
+++ 2_vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodoItem">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodoItem" />
     <span class="addContainer" v-on:click="addTodoItem">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고
+        <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">내용을 입력하세요.</div>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
 export default {
-    data:function(){
-        return{
-            newTodoItem:'',
-        }
+  components: { Modal },
+  data: function () {
+    return {
+      newTodoItem: "",
+      showModal: false,
+    };
+  },
+  methods: {
+    addTodoItem: function () {
+      if (!this.newTodoItem) {
+        this.showModal = !this.showModal;
+      } else {
+        this.$emit("addTodo", this.newTodoItem);
+        this.clearInput();
+      }
     },
-    methods:{
-        addTodoItem:function(){
-            if(!this.newTodoItem) return;
-            this.$emit('addTodo', this.newTodoItem);
-            this.clearInput();
-        },
-        clearInput:function(){
-            this.newTodoItem = '';
-        }
-    }
-}
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -43,7 +57,7 @@ input:focus {
 }
 .addContainer {
   float: right;
-  background: linear-gradient(to right, #6478FB, #8763FB);
+  background: linear-gradient(to right, #6478fb, #8763fb);
   display: block;
   width: 3rem;
   border-radius: 0 5px 5px 0;
@@ -51,5 +65,8 @@ input:focus {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
