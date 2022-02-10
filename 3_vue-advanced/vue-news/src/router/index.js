d5c71c47cc334df00b6261ede0149f1dce1f92a6
subin <subin9033@gmail.com>
2022-02-11 06:37:40 +0900

    Update git status

---
+++ 3_vue-advanced/vue-news/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    routes: [
+        // {
+        //     //path:url주소
+        //     path: '',
+        //     //component:url주소로 이동시 표시될 컴포넌트
+        //     component: ''
+        // }
+    ]
+})
